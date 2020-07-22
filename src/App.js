c96e68c3773170a992fb0755e5f5902d8b018a25
Carl Redding <credding07@gmail.com>
2020-07-22 15:59:40 -0400

    cleaned up routing so basic shell can render.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 // getting all the kids in the pool
 import Nav from "./nav_footer/nav";
 import Router from "./router/router";
-import Landing from "./landing/landing";
 
 class App extends React.Component {
   render() {
@@ -11,7 +10,6 @@ class App extends React.Component {
       <div className='App'>
         <Nav />
         <Router />
-        <Landing />
       </div>
     );
   }
--- src/router/router.js
+++ src/router/router.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import About from "../about";
-
+import Landing from "../landing/landing";
 class Router extends React.Component {
   render() {
     return (
       <div>
         <Switch>
+          <Route exact path='/' component={Landing} />
           {/* to about page */}
           <Route exact path='/about' component={About} />
         </Switch>
