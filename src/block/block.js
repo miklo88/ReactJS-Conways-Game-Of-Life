7c86d03df8456fe1c500f392fcf282740762992d
Carl Redding <credding07@gmail.com>
2020-07-29 22:42:30 -0400

    got minimal block aka cell styling doone on the element.

--- src/block/block.js
+++ src/block/block.js
@@ -1,6 +1,10 @@
 import React from "react";
-
+import "./block.scss";
 class Block extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
     return <div className='block-component'></div>;
   }
--- src/grid/grid.js
+++ src/grid/grid.js
@@ -1,169 +1,123 @@
 import React from "react";
 // block component
 import Block from "../block/block";
-
 // styles
 import "./grid.scss";
-import Logic from "../utils/logic";
 // Grid Component
 class Grid extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // display: "grid",
-      logic: new Logic(),
-      // change to => default grid size 25,25
-      size: [25, 25],
-      gameOn: false,
-      generation: 0,
-    };
-    //binding state to handleChanges
-    this.handleColChange = this.handleColChange.bind(this);
-    this.handleRowChange = this.handleRowChange.bind(this);
-    this.startGame = this.startGame.bind(this);
-    this.stopGame = this.stopGame.bind(this);
-    // this.clearGame = this.clearGame.bind(this);
-    this.renderGame = this.renderGame.bind(this);
+  constructor() {
+    super();
+    this.state = { display: "grid" };
   }
-  // event handler crew
-  // input column change
-  handleColChange(e) {
-    if (!this.state.gameOn) {
-      let actualSize = this.state.size;
-      if (e.target.value < 25) {
-        actualSize[0] = e.target.value;
-      } else {
-        actualSize[0] = 25;
-      }
-      this.setState({
-        size: actualSize,
-      });
-      this.renderGame();
-    }
-  }
-  //input row change
-  handleRowChange(e) {
-    if (!this.state.gameOn) {
-      let actualSize = this.state.size;
-      if (e.target.value < 25) {
-        actualSize[1] = e.target.value;
-      } else {
-        actualSize[1] = 25;
-      }
-      this.setState({
-        size: actualSize,
-      });
-      this.renderGame();
-    }
-  }
-  // starting the game of life BUTTONS
-  startGame() {
-    if (!this.state.gameOn) {
-      this.setState({ gameOn: true }, () => {
-        this.generationRef = setInterval(
-          () => this.runGame(),
-          this.state.generation
-        );
-      });
-    }
-  }
-  stopGame() {
-    this.setState(
-      {
-        gameOn: false,
-      },
-      () => {
-        if (this.generationRef) {
-          clearInterval(this.generationRef);
+  render() {
+    // //changing grid state
+    setTimeout(() => {
+      this.setState({ display: grid });
+    }, 60000);
+    //  grid creation logic
+    // let newArray = []
+    let cols = 5;
+    let rows = 5;
+    let grid = initArray(cols, rows);
+    console.log("Grid Array");
+    console.table(grid);
+    // grid creation function
+    function initArray(cols, rows) {
+      let carlsGrid = [];
+      for (let i = 0; i < cols; i++) {
+        //array declaration
+        carlsGrid[i] = [];
+        for (let j = 0; j < rows; j++) {
+          //subarray declaration
+          carlsGrid[i][j] = Math.floor(Math.random() * 2);
         }
       }
-    );
-  }
-  // clearGame() {}
-  runGame() {
-    this.setState({
-      logic: this.state.logic.addGeneration(),
-    });
-  }
-
-  renderGame() {
-    let newGame = [];
-    let gridRow = [];
-    for (let i = 0; i < this.state.size[0]; i++) {
-      for (let j = 0; j < this.state.size[1]; j++) {
-        gridRow.push(<Block key={[i, j]} />);
+      return carlsGrid;
+    }
+    console.log("intitArray func", initArray(cols, rows));
+    // being able to tell if a grid cell is dead or alive
+    function deadAlive(grid) {
+      let dead = false;
+      let alive = true;
+      for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid.length; j++) {
+          // console.log(grid[i]);
+          if (grid[i][j] === 1) {
+            // console.log(alive);
+            return alive;
+          } else {
+            // console.log(dead);
+            return dead;
+          }
+        }
       }
-      newGame.push(
-        <div className='grid=row' key={i}>
-          {gridRow}
-        </div>
-      );
-      gridRow = [];
+      // renaming the grid creation function
+      let newArray = initArray(cols, rows);
+      //looping through the grid
+      for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+          //renaming the myNeighbors function
+          let amigos = myNeighbors(grid, i, j);
+          //where the RULES OF LIFE ARE APPLIED
+          if (newArray[i][j] === dead && amigos === 3) {
+            return newArray[i][j] === alive;
+          } else if (newArray[i][j] === alive && (amigos < 2 || amigos > 3)) {
+            return newArray[i][j];
+          } else {
+            return newArray[i][j];
+          }
+        }
+      }
+      console.log("newArray", newArray);
+      return newArray === grid;
     }
-    return newGame;
-  }
-
-  changeGeneration = (e) => {
-    if (!this.state.gameOn) {
-      this.setState({
-        generation: e.target.value,
-      });
+    // console.log(newArray);
+    deadAlive(grid);
+    console.log("deadAlive func", deadAlive(grid));
+    // neighbors
+    function myNeighbors(grid, x, y) {
+      //loopin back through the array to find neighbors
+      for (let i = 0; i < x; i++) {
+        for (let j = 0; j < y; j++) {
+          // array elements surrouding current element.
+          let neighbors = [
+            // // above
+            [i - 1, j - 1],
+            [i, j - 1],
+            [i + 1, j - 1],
+            // // left side
+            [i - 1, j],
+            // // middle
+            // [i, j],
+            //right side
+            [i + 1, j],
+            // // below
+            [i - 1, j + 1],
+            [i, j + 1],
+            [i + 1, j + 1],
+          ];
+          return neighbors;
+        }
+      }
     }
-  };
-
-  render() {
+    myNeighbors(grid, 5, 5);
+    console.log("myNeighbors func", myNeighbors(grid, 5, 5));
     //component render
     return (
-      //   <Block />
-      // UI for the game
       <div className='grid-component'>
-        <h1>Conways Game of Life</h1>
-
-        <label className='label'>
-          Rows
-          <input
-            className='input'
-            type='text'
-            value={this.state.size[1]}
-            onChange={this.handleRowChange}
-          />
-        </label>
-        <label className='label'>
-          Columns
-          <input
-            className='input'
-            type='text'
-            value={this.state.size[0]}
-            onChange={this.handleColCHange}
-          />
-        </label>
-        <div className='generation'>
-          {/* get state for generation aka count {this.state.logic.getGeneration()}*/}
-          Generation:
-        </div>
-        {/* GRID COMPONENT */}
-        {/* <div className='grid'>{this.state.display}</div> */}
-        {this.renderGame()}
-        {/* GRID COMPONENT */}
-        {/* BUTTONS */}
-        <div className='button-group'>
-          {/* START */}
-          <button className='btn start' onClick={this.startGame}>
-            Start
-          </button>
-          {/* STOP */}
-          <button className='stop btn' onClick={this.stopGame}>
-            Stop
-          </button>
-          {/* CLEAR */}
-          <button
-            className='clear btn'
-            // onClick={this.clearGame}
-          >
-            Clear
-          </button>
-          <div>Speed:</div>
-        </div>
+        <div className='grid'>{this.state.display}</div>
+        {/* start */}
+        <button
+        // value={this.props.value}
+        // onChange={this.handleChange.bind(this)}
+        >
+          Start
+        </button>
+        {/* stop */}
+        <button>Stop</button>
+        {/* clear */}
+        <button>Clear</button>
+        <Block />
       </div>
     );
   }
