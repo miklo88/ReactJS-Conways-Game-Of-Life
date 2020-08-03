7ecd77c5dc742775f912b9914fc3070097192a49
Carl Redding <credding07@gmail.com>
2020-08-03 00:07:41 -0500

    cleaning up files. remembered to change the app and app style file to lowercase because heroku loves to throw a fit.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
-import "./App.scss";
+import "./app.scss";
 // getting all the kids in the pool
 import Nav from "./nav_footer/nav";
 import Router from "./router/router";
+import Footer from "./nav_footer/footer";
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
+        {/* components */}
         <Nav />
         <Router />
+        <Footer />
       </div>
     );
   }
